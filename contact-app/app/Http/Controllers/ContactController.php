8707b3f31b6167c0d7d4f3a8210b7604fc0cbfab
Eduardo <egbulsoni@gmail.com>
2023-10-17 18:32:21 -0300

    forms crud

--- contact-app/app/Http/Controllers/ContactController.php
+++ contact-app/app/Http/Controllers/ContactController.php
@@ -11,7 +11,7 @@ class ContactController extends Controller
     public function index()
     {
         $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
-        $contacts = Contact::orderBy('first_name', 'asc')->where(function ($query) {
+        $contacts = Contact::orderBy('first_name', 'desc')->where(function ($query) {
             if ($companyId = request('company_id')) {
                 $query->where('company_id', $companyId);
             }
@@ -21,12 +21,58 @@ class ContactController extends Controller
 
     public function create()
     {
-        return view('contacts.create');
+        $contact = new Contact();
+        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
+        return view('contacts.create', compact('companies', 'contact'));
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'company_id' => 'required|exists:companies,id',
+        ]);
+        // dd($request->all());
+        Contact::create($request->all());
+
+        return redirect()->route('contacts.index')->with('message', "Contact has been added successfully");
+    }
+    public function edit($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
+        return view('contacts.edit', compact('companies', 'contact'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'company_id' => 'required|exists:companies,id',
+        ]);
+        // dd($request->all());
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+
+        return redirect()->route('contacts.index')->with('message', "Contact has been updated successfully");
+    }
     public function show($id)
     {
-        $contact = Contact::find($id);
+        $contact = Contact::findOrFail($id);
         return view('contacts.show', compact('contact'));
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return back()->with('message', "Contact has been deleted successfully");
     }
 }
